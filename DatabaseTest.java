4e983220fb82879642a0992b0e6f0905c0763b09
chochua <chochua@iowa.uiowa.edu>
2014-10-23 12:01:52 -0500

    Update 0.4
    added DatabaseTest

---
+++ DatabaseTest.java
@@ -0,0 +1,72 @@
+package org.uiowa.cs2820.engine;
+import static org.junit.Assert.*;
+import java.util.Set;
+import java.util.HashSet;
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	@Test
+	public void testConstructor() {
+		// testing the Constructor and isEmpty
+		Database T = new Database();
+		assertEquals(T.isEmpty(),true);
+	}
+	
+	@Test
+	public void testContain() {
+		// testing whether path in database
+		Database T = new Database();
+		T.add("amount", "900");
+		assertEquals(T.containsPath("amount"),true);
+	}
+	
+	@Test
+	public void testAdd() {
+		// testing the add and get
+		Database T = new Database();
+		T.add("amount", "900");
+		assertEquals(T.get("900"),"amount");
+	}	
+
+	@Test
+	public void testRemove() {
+		// testing the remove
+		Database T = new Database();
+		T.add("amount", "900");
+		T.add("sku", "12345");
+		T.remove("amount", "900");
+		assertEquals(T.get("900"),null);
+	}
+	
+	@Test
+	public void testSize() {
+		// testing the size
+		Database T = new Database();
+		T.add("amount", "900");
+		assertEquals(T.size(),1);
+	}
+	
+	@Test
+	public void testReset() {
+		// testing the reset
+		Database T = new Database();
+		T.add("amount", "900");
+		T.add("sku", "12345");
+		T.reset();
+		assertEquals(T.size(),0);
+	}
+	
+	@Test
+	public void testShowAll() {
+		// testing the show all
+		Database T = new Database();
+		T.add("amount", "900");
+		T.add("sku", "12345");
+		Set S = new HashSet();
+		S.add("amount");
+		S.add("sku");
+		assertEquals(T.showallPath(),S);
+	}
+	
+}
--- Indexer.java
+++ Indexer.java
@@ -1,19 +1,17 @@
+
 package org.uiowa.cs2820.engine;
 
+import org.uiowa.cs2820.engine.Database;
+import org.uiowa.cs2820.engine.Field;
+
 public class Indexer {
+	Indexer(id) {
+		return;
+	    }
+	add(Field newf) { 
+		}
 
-	public Indexer(String identifier) {
-		// TODO Auto-generated constructor stub
+	close() {
+		}
+
 	}
-
-	public void add(Field f) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void close() {
-		// TODO Auto-generated method stub
-		
-	}
-
-}
